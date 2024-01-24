34de6ad3201765ad289aebc6a2193bbe1125fcff
chazkelly <124052033+chazkelly@users.noreply.github.com>
2024-01-24 09:07:23 +0000

    added batch file to install all modules

---
+++ setup.bat
@@ -0,0 +1,23 @@
+@echo off
+echo Installing required Python packages...
+pip install win32gui
+pip install ctypes
+pip install time
+pip install threading
+pip install opencv-python
+pip install numpy
+pip install pyautogui
+pip install tkinter
+pip install keyboard
+pip isntall os
+pip install sys
+pip install pyperclip
+pip install requests
+pip install json
+pip install pillow
+pip install win32api
+pip install win32con
+pip install discord
+
+
+echo Installation complete.
